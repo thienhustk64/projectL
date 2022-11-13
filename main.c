f769db73496a56a0e6aa632aa6eaeafddddc5fec
ducthien1005@gmail.com <ducthien1005@gmail.com>
2022-11-13 15:51:52 +0700

    fix library

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
-#include<SDL2/SDL.h>
-#include<SDL2/SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include<time.h>
 
 const int WIDTH = 800, HEIGHT = 600, FPS = 30;
+SDL_Renderer *renderer;
+SDL_Window *window;
 
 typedef struct{
     int x, y;
     short life;
     char *name;
 }Man;
-
 
 typedef struct{
     Man man;
@@ -19,6 +23,31 @@ typedef struct{
     SDL_Texture *character;
     SDL_Renderer *renderer;
 }GameState;
+
+void initGame(){
+    SDL_Init(SDL_INIT_EVERYTHING);
+    window = SDL_CreateWindow("Project L", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    if(window == NULL){
+        printf("Window could not initialize! SDL_Error: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == NULL){
+        printf("Cant create render! SDL_Error: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    int imgFlags = IMG_INIT_PNG;
+    if(!(IMG_Init(imgFlags) &imgFlags)){
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        exit(1);
+    }
+    if(TTF_Init() == -1){
+        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+        exit(1);
+    }
+}
 
 void loadGame( GameState *game){
     int i;
@@ -34,6 +63,8 @@ void loadGame( GameState *game){
     SDL_FreeSurface( surface);
     game->man.x = 320-40;
     game->man.y = 240-40;
+
+    // Detect animation
     for( i = 0; i < 6; i++){
         game->imgSize[i].x = 48*i;
         game->imgSize[i].y = 0;
@@ -42,7 +73,8 @@ void loadGame( GameState *game){
     }
 }
 
-void loadBackground( SDL_Window *window){
+// Load menu game
+void loadMenu(){
     SDL_Surface *screen;
     SDL_Surface *background;
     screen = SDL_GetWindowSurface( window);
@@ -57,7 +89,7 @@ void loadBackground( SDL_Window *window){
     SDL_UpdateWindowSurface( window);
 }
 
-int processEvents(SDL_Window *window, GameState *game){
+int processEvents(GameState *game){
     SDL_Event windowEvent;
     int done = 0;
     while( SDL_PollEvent( &windowEvent)){
@@ -113,17 +145,19 @@ void doRender( GameState *game, int *frame){
     SDL_Rect mapRect = { 0, 0, WIDTH, HEIGHT};
     SDL_RenderCopy( game->renderer, game->map, NULL, &mapRect);
 
-    SDL_Rect characterRect = { game->man.x, game->man.y, 48, 48};
+    // Load animation
+    SDL_Rect characterRect = { game->man.x, game->man.y, 64, 64};
     if( *frame >= 6){
         *frame = 0;
     }
     SDL_RenderCopyEx( game->renderer, game->character, &game->imgSize[*frame], &characterRect, 0, NULL, 0);
     *frame = *frame + 1;
+
     // show after you drawing
     SDL_RenderPresent(game->renderer);
 }
 
-void destroy( SDL_Window *window, SDL_Renderer *renderer, GameState *game){
+void destroy(GameState *game){
     SDL_DestroyTexture( game->character);
     SDL_DestroyTexture( game->map);
     SDL_DestroyWindow( window);
@@ -131,28 +165,18 @@ void destroy( SDL_Window *window, SDL_Renderer *renderer, GameState *game){
 }
 
 int main(int argc, char *argv[]) {
-
     GameState gameState;
-    SDL_Renderer *renderer;
-    SDL_Window *window;
     Uint32 start;
 
-    SDL_Init(SDL_INIT_EVERYTHING);
-    window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-    renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    initGame( window, renderer);
     gameState.renderer = renderer;
-
-    if( NULL == window){
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
 
     loadGame( &gameState);
     int done = 0;
     int frame = 0;
     while( !done) {
         start = SDL_GetTicks();
-        done = processEvents( window, &gameState);
+        done = processEvents( &gameState);
         doRender( &gameState, &frame);
         // Tránh CPU xử lý nhiều
         if(1000/FPS > SDL_GetTicks()-start) {
@@ -161,7 +185,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Wait a few second before quitting
-    destroy( window, renderer, &gameState);
+    destroy( &gameState);
     SDL_Quit();
     return EXIT_SUCCESS;
 }
