af5c0d986e84c02a2dd66d93e41bd34000b9c34c
ducthien1005@gmail.com <ducthien1005@gmail.com>
2022-11-16 08:52:06 +0700

    update ToDo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <SDL2/SDL_ttf.h>
 #include<time.h>
 
-const int WIDTH = 800, HEIGHT = 600, FPS = 30;
+const int WIDTH = 800, HEIGHT = 600, FPS = 20;
 SDL_Renderer *renderer;
 SDL_Window *window;
 
@@ -14,13 +14,14 @@ typedef struct{
     int x, y;
     short life;
     char *name;
+    short action;
+    SDL_Texture *walk;
+    SDL_Rect walkStatus[6];
 }Man;
 
 typedef struct{
     Man man;
-    SDL_Rect imgSize[6];
     SDL_Texture *map;
-    SDL_Texture *character;
     SDL_Renderer *renderer;
 }GameState;
 
@@ -58,35 +59,21 @@ void loadGame( GameState *game){
     game->map = SDL_CreateTextureFromSurface( game->renderer, surface);
     SDL_FreeSurface( surface);
 
-    surface = IMG_Load( "character/Biker/Walk1.png");
-    game->character = SDL_CreateTextureFromSurface( game->renderer, surface);
+    surface = IMG_Load( "character/ichigo2(60x90).png");
+    game->man.walk = SDL_CreateTextureFromSurface( game->renderer, surface);
     SDL_FreeSurface( surface);
+
+    game->man.action = 0;
     game->man.x = 320-40;
     game->man.y = 240-40;
 
     // Detect animation
-    for( i = 0; i < 6; i++){
-        game->imgSize[i].x = 48*i;
-        game->imgSize[i].y = 0;
-        game->imgSize[i].w = 48;
-        game->imgSize[i].h = 48;
+    for( i = 0; i < 4; i++){
+        game->man.walkStatus[i].x = 90*i;
+        game->man.walkStatus[i].y = 0;
+        game->man.walkStatus[i].w = 90;
+        game->man.walkStatus[i].h = 60;
     }
-}
-
-// Load menu game
-void loadMenu(){
-    SDL_Surface *screen;
-    SDL_Surface *background;
-    screen = SDL_GetWindowSurface( window);
-    background = IMG_Load("map/Background.png");
-    if( background == NULL){
-        printf("Cant find image\n");
-        SDL_Quit();
-        exit(1);
-    }
-    SDL_BlitSurface( background, NULL, screen, NULL);
-    SDL_FreeSurface( background);
-    SDL_UpdateWindowSurface( window);
 }
 
 int processEvents(GameState *game){
@@ -119,13 +106,14 @@ int processEvents(GameState *game){
         }
     }
 
-    
     const Uint8 *state = SDL_GetKeyboardState(NULL);
     if( state[ SDL_SCANCODE_RIGHT]){
         game->man.x += 10;
+        game->man.action = 0;
     }
     if( state[ SDL_SCANCODE_LEFT]){
         game->man.x -= 10;
+        game->man.action = 1;
     }
     if( state[ SDL_SCANCODE_UP]){
         game->man.y -= 10;
@@ -146,11 +134,15 @@ void doRender( GameState *game, int *frame){
     SDL_RenderCopy( game->renderer, game->map, NULL, &mapRect);
 
     // Load animation
-    SDL_Rect characterRect = { game->man.x, game->man.y, 64, 64};
-    if( *frame >= 6){
+    SDL_Rect walkRect = { game->man.x, game->man.y, 64, 64};
+    if( *frame >= 4){
         *frame = 0;
     }
-    SDL_RenderCopyEx( game->renderer, game->character, &game->imgSize[*frame], &characterRect, 0, NULL, 0);
+    if( game->man.action == 0){
+        SDL_RenderCopyEx( game->renderer, game->man.walk, &game->man.walkStatus[*frame], &walkRect, 0, NULL, SDL_FLIP_NONE);
+    }else if( game->man.action == 1){
+        SDL_RenderCopyEx( game->renderer, game->man.walk, &game->man.walkStatus[*frame], &walkRect, 0, NULL, SDL_FLIP_HORIZONTAL);
+    }
     *frame = *frame + 1;
 
     // show after you drawing
@@ -158,7 +150,7 @@ void doRender( GameState *game, int *frame){
 }
 
 void destroy(GameState *game){
-    SDL_DestroyTexture( game->character);
+    SDL_DestroyTexture( game->man.walk);
     SDL_DestroyTexture( game->map);
     SDL_DestroyWindow( window);
     SDL_DestroyRenderer( renderer);
